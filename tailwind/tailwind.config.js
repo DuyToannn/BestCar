371d523282977200ef82dc187aef78a66f61ae57
Duy Toan <toanpmo123@gmail.com>
2021-11-22 08:26:41 +0700

    up

--- tailwind/tailwind.config.js
+++ tailwind/tailwind.config.js
@@ -4,8 +4,34 @@ module.exports = {
     theme: {
         extend: {
             colors: {
-                blues33: '#333',
+                blue00: '#005383',
+                yellowd3: '#d3d126'
             },
         },
     },
+    plugins: [
+        function ({ addComponents }) {
+          addComponents({
+            '.container': {
+              width: '100%',
+            //   marginLeft: 'auto',
+            //   marginRight: 'auto',
+            //   paddingLeft: '2rem',
+            //   paddingRight: '2rem',
+              '@screen sm': {
+                maxWidth: '640px',
+              },
+              '@screen md': {
+                maxWidth: '767px',
+              },
+              '@screen lg': {
+                maxWidth: '1024px',
+              },
+              '@screen xl': {
+                maxWidth: '1280px',
+              },
+            }
+          })
+        }
+      ]
 }
